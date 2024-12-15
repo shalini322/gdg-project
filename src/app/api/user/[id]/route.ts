3a59ee5b19fa9fee6e1e672d1b8b85341d797b7d
Ayushman Rana <ayushmanr621@outlook.com>
2024-12-16 00:41:22 +0530

    ..

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,5 +1,5 @@
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
@@ -22,5 +22,26 @@ export async function GET(
       { message: "Error fetching user", error },
       { status: 500 }
     );
+  }
+}
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+
+    console.log("Received Data:", data);
+
+    // Respond with a success message
+    return NextResponse.json(
+      { message: "Data received successfully", data },
+      { status: 200 }
+    );
+  } catch (error) {
+    //Error handling
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { error: "Invalid request", details: error.message },
+        { status: 400 }
+      );
+    }
   }
 }
